cadf40dec027795f407931a9b033d22cb5ee537b
HamzaRomdhani <hamzajaa13@gmail.com>
2023-06-11 16:36:13 +0200

    validation with class generic for fields

--- src/main/java/com/learn/banking/DTOs/AccountRequest.java
+++ src/main/java/com/learn/banking/DTOs/AccountRequest.java
@@ -1,5 +1,8 @@
 package com.learn.banking.DTOs;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
@@ -10,7 +13,11 @@ import lombok.*;
 public class AccountRequest {
 
 
+    @NotNull(message = "this iban should not be null")  //Null
+    @NotBlank(message = "this iban should not be blank") // ""
+    @NotEmpty(message = "this iban should not be empty") // " "
     private String iban;
 
+    @NotNull(message = "this user id  should not be null")
     private Integer userId;
 }
--- src/main/java/com/learn/banking/service/AccountService.java
+++ src/main/java/com/learn/banking/service/AccountService.java
@@ -4,6 +4,7 @@ import com.learn.banking.DTOs.AccountRequest;
 import com.learn.banking.entity.Account;
 import com.learn.banking.mappers.AccountMapper;
 import com.learn.banking.repository.AccountRepository;
+import com.learn.banking.validations.ObjectValidation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,11 @@ public class AccountService {
     private final AccountRepository repository;
     private final AccountMapper mapper;
 
+    private final ObjectValidation<AccountRequest> validator;
+
     public Integer saveAccount(AccountRequest accountreq){
+
+        validator.validate(accountreq);
 
         var account = mapper.toAccount(accountreq);
 
